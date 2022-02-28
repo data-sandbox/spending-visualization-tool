de25abe62d6b5db6da25aa95f71b407c88fd3c7b
C <99933422+data-science-sandbox@users.noreply.github.com>
2022-02-28 18:48:30 -0500

    Group by category and date to remove duplicate entries in dataframe. Delete obsolete commented code

--- spending-visualization-tool.py
+++ spending-visualization-tool.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Initial release: 2022 02 22
+Initial release: 2022 02
 
 """
 
@@ -13,22 +13,15 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-### INPUTS, BEGIN
-
-# year, month
-# date_start = 2101
-# date_end = 2112
-# date_range = np.arange(date_start, date_end + 1, 1)
-
-### INPUTS, END
-
+#%%
 # get all csv files in directory
 path = os.getcwd()
 csv_files = glob.glob(os.path.join(path, "*.csv"))
 
+# generate empty dataframe
 spending_output_df = pd.DataFrame()
-  
-# loop over the list of csv files
+
+#%% iterate through list of csv files
 for f in csv_files:
     
     # read the month
@@ -38,39 +31,13 @@ for f in csv_files:
     # read the csv file
     spending_raw_df = pd.read_csv(f)
     
-    # generate dummy dataframe with all categories of interest
-    # categories = pd.read_csv('categories.csv')
-    # # add other columns
-    # categories['Debit'] = 0
-    # categories['Credit'] = 0
-    # categories['Net'] = 0
-        
-    # temporary dataframe to pull from
-    # if account[i] == 'account1' and exists(file_name) == False:
-    #     print('')
-    #     print('No account1 statement')
-    #     spending_raw_df = categories
-    # elif account[i] == 'account2' and exists(file_name) == False:
-    #     print('')
-    #     print('No account2 statement')
-    #     spending_raw_df = categories
-    # elif account[i] == 'account3':
-    #     # skip first few rows
-    #     spending_raw_df = pd.read_csv(file_name, skiprows=3)
-    # else:
-    #     # skip no rows
-    #     spending_raw_df = pd.read_csv(file_name)
-    
-    ### clean csv if header rows included
+    # clean csv if header rows included
     date_check = ['Date', 'Post Date', 'Posted Date']
     # reset flag
     date_flag = 0 
     for d in date_check:
         if d in spending_raw_df.columns:
             date_flag = 1
-            # skip rows
-            #print(d)
-            #print(spending_raw_df)
     if date_flag == 0:
         # header rows need to be cleaned    
         spending_raw_df = pd.read_csv(f, skiprows=3)
@@ -86,34 +53,17 @@ for f in csv_files:
     if 'Credit' not in spending_raw_df.columns:
          spending_raw_df['Credit'] = 0
 
-    # error checks
-    # column_check = ['Debit', 'Credit']
-    # for column in column_check:
-    #     column in spending_raw_df.columns
-    #     assert column > 0, "column not present"
-    
-    # column_check = 'Debit' in dfTemp1.columns
-    # assert column_check > 0, "Debit column not present"
-    # column_check = 'Credit' in dfTemp1.columns
-    # assert column_check > 0, "Credit column not present"
-
-    # force columns to be consistently positive
+    # clean columns to be all positive
     spending_raw_df['Debit'] = spending_raw_df['Debit'].abs()
     spending_raw_df['Credit'] = spending_raw_df['Credit'].abs()
 
-    # replace all nan values with zeros for later math operations
+    # clean nan values with zeros for later math operations
     spending_raw_df = spending_raw_df.fillna(value={'Debit': 0, 'Credit': 0})
-
-    # add amount data to temporary dataframe summary
-    #spending_raw_df = pd.concat([spending_raw_df, spending_raw_df], axis=0)
         
     # add net column
     spending_raw_df['Net'] = spending_raw_df['Debit'] - spending_raw_df['Credit']
     # absolute value net column
     spending_raw_df['Net'] = spending_raw_df['Net'].abs()
-    
-    # add category list to include any categories with zero spending
-    #spending_raw_df = pd.concat([spending_raw_df, categories])
     
     # keep only category and amount columns
     spending_raw_df = spending_raw_df[['Category', 'Debit', 'Credit', 'Net']]
@@ -124,41 +74,22 @@ for f in csv_files:
     # add column identifying the year and month
     dfMonth.insert(1, 'Date', month)
     
-    # determine name of running log data file
-    # month = date_end % 100
-    # if month < 2:
-    #     # account for last entry being the prior year
-    #     log_date = date_end - 89
-    # else:
-    #     log_date = date_end - 1
-    
-    # read running log data
-    #spending_log_df = pd.read_csv('log' + str(log_date) + '.csv')
-    
-    # keep only category and amount columns
-    #spending_log_df = spending_log_df[['Category', 'Date', 'Debit', 'Credit', 'Net']]
-    
     # add monthly data to running annual data
-    #spending_output_df = pd.concat([spending_log_df, dfMonth])
     spending_output_df = pd.concat([spending_output_df, dfMonth])
     
     # reset index
     spending_output_df = spending_output_df.reset_index(drop=True)
-    # export new log
-    #spending_output_df.to_csv('log' + str(date_end) + '.csv')
 
-# compute running totals
-#dfTotals = spending_output_df.groupby('Category', as_index=False).agg('sum')
+#%% data cleaning
+# group by category for each month
+spending_output_df = spending_output_df.groupby(['Date', 'Category'], as_index=False).sum()
 
-# compute expense totals only
-# spending_expenses_df = ['income', 'freelance', 'investment']
-# for i in spending_expenses_df:
-#     dfTotals = dfTotals[dfTotals['Category'].str.contains(spending_expenses_df[i])==False]
-# totalExp = dfTotals['Net'].sum()
+#spending_output_df.sort_values('Date')
 
 # convert date to string so Dec to Jan dates still plot side by side
-spending_output_df['Date'] = spending_output_df['Date'].astype(str)
+#spending_output_df['Date'] = spending_output_df['Date'].astype(str)
 
+#%% plot function
 
 def plot_summary(data_all, categories1, categories2):
     """
@@ -195,7 +126,11 @@ def plot_summary(data_all, categories1, categories2):
     data_subset1 = data_all.loc[data_all['Category'].isin(categories1)]
     data_subset2 = data_all.loc[data_all['Category'].isin(categories2)]
     
-    data_tuple1 = tuple[categories1, data_subset1]
+    # force sort by date?
+    # data_subset1.sort_values('Date')
+    # data_subset2.sort_values('Date')
+    
+    #data_tuple1 = tuple[categories1, data_subset1]
     
     # overview plot
     plt.close('all')
@@ -231,6 +166,7 @@ def plot_summary(data_all, categories1, categories2):
     plt.xticks(rotation=label_angle)
     plt.show()
 
+#%% run function
 
 if __name__ == "__main__":
     
